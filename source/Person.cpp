85f75325ca020345ea389360356bae722224ebc1
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-05-14 18:10:10 -0400

    Implemented setters

--- source/Person.cpp
+++ source/Person.cpp
@@ -7,3 +7,20 @@ using namespace std;
 Person::Person(const string& name, const string& lastName, int age)
     : name(name), lastName(lastName), age(age) {}
 
+// Setter implementations
+void Person::SetDOB(string& date) {
+    DOB = date;
+}
+
+void Person::SetCasualEmail(string& email) {
+    casualEmail = email;
+}
+
+void Person::SetProEmail(string& email) {
+    proEmail = email;
+}
+
+void Person::SetAge(int age) {
+    this->age = age;
+}
+
