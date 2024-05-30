d6b57b9ae5794502ccd30ce5903ebed97f93cf5b
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-05-30 08:31:16 -0400

    More tests, things working

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,14 @@
 #include "Person.h"
+#include <iostream>
 
 int main() {
     Person Simon("Simon", "Lavoie", 23);
+    std::string name = "test";
+    Simon.SetDOB(name);
+    std::cout << Simon.GetDOB() << std::endl;
 
+    Person Test("fname", "lname", "lol");
+    std::cout << Test.GetDOB() << std::endl;
+    std::cout << Test.age << std::endl;
     return 0;
 }
