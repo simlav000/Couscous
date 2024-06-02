f5417f78b140a28a9d52808018eeb74af037bbdf
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-06-02 19:39:33 -0400

    more tests

--- source/main.cpp
+++ source/main.cpp
@@ -7,7 +7,7 @@ int main() {
     Simon.SetDOB(name);
     std::cout << Simon.GetDOB() << std::endl;
 
-    Person Test("fname", "lname", "lol");
+    Person Test("fname", "lname", "2000-11-30");
     std::cout << Test.GetDOB() << std::endl;
     std::cout << Test.age << std::endl;
     return 0;
